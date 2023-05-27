77d2be4fe0e57f93fb9a37ece82f446b61991813
Antoine Plard <antoineplddev@gmail.com>
2023-05-27 19:17:41 +0200

    extract client from project

--- phlegyas/mq/write.js
+++
@@ -1,9 +0,0 @@
-const {COMMAND_QUEUE} = require("./connector");
-
-function sendCommand(channel, msg) {
-    channel.sendToQueue(COMMAND_QUEUE, Buffer.from(msg));
-}
-
-module.exports = {
-    sendCommand: sendCommand
-}
